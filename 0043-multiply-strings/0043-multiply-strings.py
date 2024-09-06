16f6e242f27b62e3fc4e970bcf1d57772b01ec62
AralAskarov <askarov.10.09@gmail.com>
2024-09-06 20:19:17 +0500

    Time: 18 ms (52.76%), Space: 11.84 MB (6.21%)

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def multiply(self, num1, num2):
+        """
+        :type num1: str
+        :type num2: str
+        :rtype: str
+        """
+        x1 = int(num1)
+        x2 = int(num2)
+        return str(x1*x2)
+        
